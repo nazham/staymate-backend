a21692e02819eb7fb3b334a3646e6686b710cdda
nazham <nazhammohamed@gmail.com>
2025-03-31 09:44:15 +0530

    Enhance CORS configuration to support credentials and specify allowed methods and headers

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://staymate-frontend.vercel.app", "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
